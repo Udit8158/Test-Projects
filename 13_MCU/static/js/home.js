5e9d6832416b283d430504b4af9e5c2b5a0aca4c
Udit8158 <kunduudit8158@gmail.com>
2022-01-23 11:42:04 +0530

    add MCU

---
+++ 13_MCU/static/js/home.js
@@ -0,0 +1,16 @@
+document.querySelector('.home').classList.add('active');
+
+const menu = document.querySelector('nav ul');
+const hamburger = document.querySelector('.mobile');
+
+hamburger.addEventListener('click', () => {
+    if (menu.style.visibility == 'hidden') {
+        console.log('clicked')
+        console.log(menu)
+        menu.style.visibility = 'visible';
+    } else {
+        console.log('clicked again')
+        console.log(menu)
+        menu.style.visibility = 'hidden';
+    }
+})
